0631a0b4dbe76466c950fafe44100ba4609eb75d
augustomatheuss <augustomatheuss@yahoo.com.br>
2015-10-03 15:36:51 -0300

    update examples

--- examples/xor_and_example.m
+++ examples/xor_and_example.m
@@ -1,20 +1,20 @@
-%% Learning XOR logical operation
+%% Learning XOR and AND logical operations
 
 % Configuration
-neurons = [3 4 1];
-a = 0.001;
+neurons = [3 4 2];
+a = 1e-4;
 bias = [-1 -1 -1];
 x = [0 0;0 1;1 0; 1 1];
-yref = [0 ;1 ;1 ;0 ];
-lr = 0.375;
-error = 10^-20;
-maxIt = 100000;
+yref = [0 0;1 0;1 0;0 1];
+lr = 0.7;
+error = 1e-20;
+maxIt = 1e5;
 
 % Processing
 [W,E] = trainingMLP(neurons,a,bias,x,yref,lr,error,maxIt);
 
 % Display Infos
-disp('XOR')
+disp('XOR AND')
 disp('Input [0 0]');
 outMLP(neurons,bias,[0 0],W)
 disp('Input [0 1]');
@@ -26,3 +26,6 @@ outMLP(neurons,bias,[1 1],W)
 
 % Plot MSE
 semilogx(mse_hist)
+ylabel('MSE');
+xlabel('Iteration');
+title('Xor and And Example','FontSize',14);
